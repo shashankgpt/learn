a6421149b7b852d1517e5c88f0694bea90d1b5c3
Shashank Gupta <shashankgpt270@gmail.com>
2024-06-27 15:27:18 +0530

    improved buble sort

--- Algo/Sorting/bubble sort/bubble-sort-array.js
+++ Algo/Sorting/bubble sort/bubble-sort-array.js
@@ -1,5 +1,5 @@
  const arr = [64, 34, 25, 12, 22, 11, 90];
-
+// always n2
 const sort = (a) => {
     let len = a.length;
     for(let i =0; i<len;i++) {
@@ -12,3 +12,24 @@ const sort = (a) => {
 }
 
 console.log(sort(arr));
+
+// will be n in best case already sorted arry
+const bubbleSort = (arr) => {
+    const n = arr.length;
+    let swapped;
+    
+    do {
+        swapped = false;
+        for (let i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                // Swap elements
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+                swapped = true;
+            }
+        }
+        // After each full pass, the largest unsorted element is at the end, so decrement n
+        n--;
+    } while (swapped);
+
+    return arr;
+};
